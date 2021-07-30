29ce930fc8c88e5b8a1a0da98ba6e26ce3559678
Oladele Joshua <jurshsmith@gmail.com>
2021-07-30 20:37:47 +0100

    simple starting of the application for a very small use case

--- kv/lib/kv.ex
+++ kv/lib/kv.ex
@@ -12,6 +12,18 @@ defmodule KV do
       :world
 
   """
+
+  # The function mix tries to start based on the 'application' registry in the mix.exs
+  # Fun fact: mix.exs is a script file that can be run on your OS level programmatically ?
+  # On execution of this function, mix wants to get a tuple of {:ok , PID } so return self() for this simple use case
+  def start _type, _args do
+    hello()
+    |> IO.puts
+
+    pid = self()
+    { :ok, pid }
+  end
+
   def hello do
     :world
   end
