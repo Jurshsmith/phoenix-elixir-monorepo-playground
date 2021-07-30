a73bc51a894838d131eb74696e991bd603811834
Oladele Joshua <jurshsmith@gmail.com>
2021-07-30 22:32:47 +0100

    adds Task as a better alternative module to Kernel's default becase it does better reporting

--- kv/lib/simple_process_com/simple_process_com_two.ex
+++ kv/lib/simple_process_com/simple_process_com_two.ex
@@ -11,11 +11,17 @@ defmodule SimpleProcessCom.Two do
   defp initialize_error_scenario_with_spawn do
     #Since processes are isolated, this will not affect parent process
     spawn(fn -> raise "oops" end)
+
+    # or use Task for better error reporting
+    Task.start(fn -> raise "oops" end)
   end
 
   defp initialize_error_scenario_with_spawn_link do
     # With spawn_link, it will affect the parent component,
     # Use case: Fault tolerance, supervisors to be alerted when a child process fails, so that the supervisor restarts those processes
     spawn_link(fn -> raise "oops" end)
+
+    #or use Task for better error reporting
+    Task.start_link(fn -> raise "oops" end)
   end
 end
