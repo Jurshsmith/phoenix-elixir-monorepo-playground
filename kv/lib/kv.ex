3825ef94abba302fbf1079e3f0bec02a50de5973
Oladele Joshua <jurshsmith@gmail.com>
2021-07-30 21:41:38 +0100

    adds scenario one where spawned process sends message to parent process even before parent process starts listening

--- kv/lib/kv.ex
+++ kv/lib/kv.ex
@@ -16,9 +16,14 @@ defmodule KV do
   # The function mix tries to start based on the 'application' registry in the mix.exs
   # Fun fact: mix.exs is a script file that can be run on your OS level programmatically ?
   # On execution of this function, mix wants to get a tuple of {:ok , PID } so return self() for this simple use case
+
+
   def start _type, _args do
-    hello()
+
+    "Started KV..."
     |> IO.puts
+
+    SimpleProcessCom.One.init()
 
     pid = self()
     { :ok, pid }
---
+++ kv/lib/simple_process_com/simple_process_com_one.ex
@@ -0,0 +1,51 @@
+defmodule SimpleProcessCom.One do
+
+  def init do
+    "Simple Process Com.One Module Init Function ..."
+    |> IO.puts
+
+    initialize_sample_scenario_one()
+  end
+
+  defp initialize_sample_scenario_one do
+    "Starting Scenario One ...."
+    |> IO.puts
+
+    create_new_simple_process_that_tells_current_process_hello()
+
+    start_listening_for_messages_and_react_accordingly()
+  end
+
+  defp create_new_simple_process_that_tells_current_process_hello do
+    # get the PID of this program's process and then
+    parent_PID = self()
+
+
+    # spawn a new process, at that point, self() refers to the process of the new spawned process,
+    spawn(
+      # this anonymous function is called immediately new process is created
+      fn ->
+        # Kernel.inspect converts anything to string.
+        parent_PID_in_string = parent_PID
+        |> Kernel.inspect
+
+        # self here will be PID for the current spawned process
+        current_spawned_process_PID = self()
+        |> Kernel.inspect
+
+        send(parent_PID, {:hello, "Hey, #{ parent_PID_in_string }, New Spawned Process You Birthed is #{current_spawned_process_PID}"})
+      end
+    )
+  end
+
+  defp start_listening_for_messages_and_react_accordingly do
+    receive do
+      {:hello, msg} ->
+        msg
+        |> IO.puts
+      _ ->
+        "Don't understand this process's message"
+        |> IO.puts
+    end
+  end
+end
