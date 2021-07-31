00415c5208dd6649bdb4c02e5222469607442681
Oladele Joshua <jurshsmith@gmail.com>
2021-07-31 14:02:06 +0100

    small update

--- phx_monorepo_umbrella/README.md
+++ phx_monorepo_umbrella/README.md
@@ -1,1 +1,22 @@
 # PhxMonorepo.Umbrella
+
+Start application with
+```
+  mix phx.server
+```
+
+View Umbrella statisitics by visiting http://localhost:4000/dashboard/home
+
+Or Run Phoenix App in interactive mode
+
+```
+  iex -S mix phx.server
+```
+
+and do
+
+```
+  :observer.start
+```
+
+to see process supervision tree of the phoenix application
--- phx_monorepo_umbrella/apps/phx_monorepo_web/lib/phx_monorepo_web.ex
+++ phx_monorepo_umbrella/apps/phx_monorepo_web/lib/phx_monorepo_web.ex
@@ -29,10 +29,6 @@ defmodule PhxMonorepoWeb do
 
   def view do
     quote do
-      use Phoenix.View,
-        root: "lib/phx_monorepo_web/templates",
-        namespace: PhxMonorepoWeb
-
       # Import convenience functions from controllers
       import Phoenix.Controller,
         only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
