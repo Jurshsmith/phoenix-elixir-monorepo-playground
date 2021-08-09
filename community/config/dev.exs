a0bd82e74c0827ae5451c4d61c24386486ea8b66
Oladele Joshua <jurshsmith@gmail.com>
2021-08-09 19:01:49 +0100

    updates the port accordingly

--- community/config/dev.exs
+++ community/config/dev.exs
@@ -6,6 +6,7 @@ config :community, Community.Repo,
   password: "postgres",
   database: "community_dev",
   hostname: "localhost",
+  port: 15432,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
